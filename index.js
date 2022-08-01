66994f75411939bb1a7264879317a50f0e07b25e
kevin-fl <kevinflamang@gmail.com>
2022-08-01 15:22:44 +0200

    update closures

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
                      // Difference expression de fonction et declaration de fonction 
+
+
 // expression de fonction le nom de la fonction peut etre omis -> on parle alors defonction anonyme 
 // les expressions de fonctions ne sont pas remontées , impossible d utiliser avant leur definition
 
@@ -169,3 +171,94 @@ function parcourirArbre(noeud) {
 }
 
 //Contrairement à l'exemple précédent avec la fonction boucle, ici, chaque appel récursif entraîne lui-même plusieurs appels (et non un seul).
+
+
+
+
+
+//*FONCTION IMBRIQUEES ET FERMETURES(CLOSURE)
+
+/*En résumé :
+
+La fonction imbriquée ne peut être utilisée qu'à partir des instructions de la fonction parente.
+La fonction imbriquée forme une fermeture : elle peut utiliser les arguments et les variables de la fonction parente. En revanche, la fonction parente ne peut pas utiliser les arguments et les variables de la fonction fille.
+*/
+
+
+//La fonction interne étant une fermeture, on peut appeler la fonction parente afin de définir les arguments pour la fonction englobante et ceux de la fonction fille :
+
+function parente(x) {
+    function fille(y) {
+      return x + y;
+    }
+    return fille;
+  }
+  fn_fille = parente(3); // Fournit une fonction qui ajoute 3 à ce qu'on lui donnera
+  résultat = fn_fille(5); // renvoie 8
+  
+  résultat1 = parente(3)(5); // renvoie 8
+
+                                                            //IMBRIQUER DES FONCTIONS 
+
+/*
+
+Il est possible d'imbriquer des fonctions sur plus de deux niveaux, par exemple, on peut avoir une fonction A qui contient une fonction B 
+qui contient une fonction C. Les fonctions B et C sont des fermetures et B peut accéder à la portée de A, C peut accéder à la portée de B.
+ Ainsi, C accède à la portée de B qui lui accède à la portée de A, C accède donc à la portée de A (transitivité).
+Les fermetures peuvent donc contenir plusieurs portées, c'est ce qu'on appelle le chaînage de portées.
+
+Par exemple :
+*/
+
+function A(x) {
+  function B(y) {
+    function C(z) {
+      console.log(x + y + z);
+    }
+    C(3);
+  }
+  B(2);
+}
+A(1); // affichera 6 (1 + 2 + 3)
+
+/*
+Dans cet exemple C accède au y de B et au x de A. Ceci est rendu possible car :
+
+B est une fermeture qui contient A, autrement dit B peut accéder aux arguments et aux variables de A.
+C est une fermeture qui contient B.
+La fermeture de B contient A donc la fermeture de C contient A, C peut ainsi accéder aux arguments et aux variables de B et A. On dit que C chaîne les portées de B et de A (dans cet ordre).
+La réciproque n'est pas vraie. A ne peut pas accéder à C, car A ne peut pas accéder aux arguments ou aux variables de B, or C est une variable de B. De cette façon, C reste privée en dehors de B.
+
+                                                                    //NOMMAGE
+
+
+Lorsque deux arguments ou variables des portées d'une fermeture ont le même nom, il y a un conflit de noms. Dans ces cas, ce sera la portée la plus imbriquée qui prendra la priorité sur le nom, la portée la plus « externe » aura la priorité la plus faible pour les noms de variables. Du point de vue de la chaîne des portées, la première portée sur la chaîne est la portée la plus imbriquée et la dernière est la portée située le plus à l'extérieur :
+*/
+function externe() {
+  var x = 10;
+  function interne(x) {
+    return x;
+  }
+  return interne;
+}
+résultat = externe()(20); // renvoie 20 et pas 10
+
+
+
+                                                        // LES CLOSURES
+
+// une fermeture donne acces a la portee d une fonction externe a partir d une fonction interne ("on dit que la fonction capture son environnement")
+//en js , une fermeture est créee chaque fois qu une fonction est créee.
+
+
+
+var animal = function(nom) {   // La fonction externe utilise un paramètre "nom"
+  var getNom = function () {
+    return nom;                // La fonction interne accède à la variable "nom" de la fonction externe
+  }
+  return getNom;               // Renvoie la fonction interne pour la rendre disponible en dehors de la portée de la fonction parente
+}
+
+monAnimal = animal("Licorne");
+
+monAnimal();                   // Renvoie "Licorne"
